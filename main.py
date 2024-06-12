2cf63c0fd66ceb7015d4a923bbd0068057999e33
nazarbodan <wolfter12@gmail.com>
2024-06-12 22:04:11 +0300

    feat: handle errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *  # noqa: F403
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 def generate_password():
@@ -81,16 +82,34 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
 
-    messagebox.askokcancel(
-        title=website,
-        message=f"These are the details entered: \nEmail: {username} \nPassword: {password} \nIs it ok to save?",
-    )
-
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {username} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops", message="Please don't leave any fields empty!"
+        )
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 window = Tk()
